9eff884a2a05c9c7832f116e0d6a6554874f444a
Victor Villas <mail@victor.villas>
2018-11-12 11:26:08 -0200

    Add setup.py with latest release

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fake-awsglue',
+    version='0.0.0.post20190320',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
