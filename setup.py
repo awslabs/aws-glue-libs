9c112107853aba5da9a9aab4934f6f0434a01ed0
Peter Tsai <tsaiweiy@amazon.com>
2026-01-12 17:01:50 -0500

    Glue 5.0 changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='aws-glue-libs',
-    version='4.0.0',
+    version='5.0.0',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
